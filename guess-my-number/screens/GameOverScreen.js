86397e5f933b8bf7c3725733c3d2f645fbf40e9e
PriontoAbdullah <priontosoft@gmail.com>
2022-06-01 21:09:11 +0600

    Added a game project

---
+++ guess-my-number/screens/GameOverScreen.js
@@ -0,0 +1,13 @@
+import { Button, Text, TextInput, View } from 'react-native';
+
+const GameOverScreen = () => {
+  return (
+    <View>
+      <Text>Game Over Screen</Text>
+      <TextInput />
+      <Button title="New Game" />
+    </View>
+  );
+};
+
+export default GameOverScreen;
